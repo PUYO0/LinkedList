ffdd5b043051185e8fd5e5a1b1fb018951eafa15
Raphael Forest <raphael.forest@telecomnancy>
2022-09-30 10:45:34 +0200

    list_rem_next

--- Linkedlist/Domain/List/list.c
+++ Linkedlist/Domain/List/list.c
@@ -28,3 +28,26 @@ int list_ins_next(List *list, ListElmt *element, const void *data)
   }
   return 0;
 }
+
+int list_rem_next(List *list, ListElmt *element, void **data)
+{
+  if (list->size == 0) return -1;
+  ListElmt *old;
+  if (element == NULL)
+  {
+    old = list->head;
+    if (old->next == 1) list->tail = NULL;
+    list->head = old->next;
+  }
+  else
+  {
+    if (element == list->tail) return -1;
+    old = element->next;
+    if (old == list->tail) list->tail = element;
+    element->next = old->next;
+  }
+  *data = old->data;
+  free(old);
+  list->size--;
+  return 0;
+}
